75b74fa00ccf95a88481826d40193b42a61fd660
eloi verspieren <eloi.verspieren@gmail.com>
2016-08-09 15:35:55 +0200

    rails new

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,42 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @tasks = Task.find(params[:id])
+  end
+
+  def new
+    @tasks = Task.new
+  end
+
+  def create
+    @tasks = Task.new(tasks_params)
+    @tasks.save
+    redirect_to tasks_path
+  end
+
+  def edit
+    @tasks = Task.find(params[:id])
+  end
+
+  def update
+    @tasks = Task.find(params[:id])
+    @tasks.update(tasks_params)
+    redirect_to tasks_path
+  end
+
+  def destroy
+    @tasks = Task.find(params[:id])
+    @tasks.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def tasks_params
+    params.require(:task).permit(:name, :content)
+  end
+end
+
